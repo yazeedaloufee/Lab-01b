2baa0972f74ad04674b587aab70ce4464e6ce23a
YAZEED ALOUFEE <YAZEEDALOUFEE@GMAIL.COM>
2021-06-20 16:37:39 +0300

    completing lab 01

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let firstName= prompt("what is you first name?")
+alert(firstName+ ". nice name!")
+let secondName = prompt("what is your second name?")
+alert("So your full name is "+firstName+" "+secondName)
+alert("with a name like "+firstName+ " "+secondName+", i think you are an actor, but let me make sure of that")
+let userJob =prompt("so "+ firstName +" what do you do for a living?")
+alert("nice!!")
+
+let userAge = prompt("how old are by the way?")
+alert("let me recape. yourname is "+ firstName+ " "+secondName+" and you are "+ userAge+" years old. and you are an actor. i mean a..."+userJob)
+console.log("yourname is "+ firstName+ " "+secondName+" and you are "+ userAge+" years old. and you are a "+userJob);
